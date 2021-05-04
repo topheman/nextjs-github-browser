63b3994d310c06d75ecae99433ca12ad692f65ee
Christophe Rosset <tophe@topheman.com>
2021-05-04 23:26:26 +0200

    feat(tailwind): setup tailwindcss
    https://tailwindcss.com/docs/guides/nextjs
    
    ```
    npm install -D tailwindcss@latest postcss@latest autoprefixer@latest
    ```
    
    Then, generate `tailwind.config.js` and `postcss.config.js`
    
    ```
    npx tailwindcss init -p
    ```
    
    For vscode intellisense, install extension : https://github.com/tailwindlabs/tailwindcss-intellisense

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -23,12 +23,12 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
+            <h2 className="text-red-700">Documentation &rarr;</h2>
             <p>Find in-depth information about Next.js features and API.</p>
           </a>
 
           <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
+            <h2 className="text-pink-700">Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
           </a>
 
@@ -36,7 +36,7 @@ export default function Home() {
             href="https://github.com/vercel/next.js/tree/master/examples"
             className={styles.card}
           >
-            <h2>Examples &rarr;</h2>
+            <h2 className="text-green-600">Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
           </a>
 
@@ -44,7 +44,7 @@ export default function Home() {
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             className={styles.card}
           >
-            <h2>Deploy &rarr;</h2>
+            <h2 className="text-indigo-600">Deploy &rarr;</h2>
             <p>
               Instantly deploy your Next.js site to a public URL with Vercel.
             </p>
