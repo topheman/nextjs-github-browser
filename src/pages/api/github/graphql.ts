edca98f1ffd0e9f8e2e39002e83ea4952cece76a
Christophe Rosset <tophe@topheman.com>
2021-05-06 20:20:30 +0200

    feat(graphql): setup proxy handler to github API /graphql

---
+++ src/pages/api/github/graphql.ts
@@ -0,0 +1,36 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+  // @warn what about clients that do GET requests ?
+  if (typeof req.method === "string" && req.method.toLowerCase() !== "post") {
+    console.warn("[GraphQL-proxy]", "Method not allowed", req.method, req.url);
+    return res.status(405).json({ error: "Method not allowed" });
+  }
+  // prevent introspection queries from graphiql and other requesters in dev mode to avoid multiple queries on rateLimit
+  if (req.body.operationName === "IntrospectionQuery") {
+    console.log("[GraphQL-proxy]", "Incoming IntrospectionQuery ignored");
+    return res.status(405).json({ error: "IntrospectionQuery not allowed" });
+  }
+  console.log("[GraphQL-proxy]", "fetching", req.method, req.url);
+  try {
+    const result = await fetch(process.env.GITHUB_GRAPHQL_API_ROOT_ENDPOINT, {
+      method: "POST",
+      body: JSON.stringify(req.body),
+      headers: new Headers({
+        Authorization: `Bearer ${process.env.GITHUB_GRAPHQL_API_TOKEN}`,
+      }),
+    });
+    if (result.ok) {
+      const response = await result.text();
+      return res.status(result.status).json(response);
+    } else {
+      throw new Error("Couldn't call github API");
+    }
+  } catch (e) {
+    // return same shaped error object
+    console.error("[GraphQL-proxy]", e.message);
+    return res.status(500).json({ errors: [{ message: e.message }] });
+  }
+};
