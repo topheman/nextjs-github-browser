a00cae0c6439e584744d2ced4fc63ee40db226a6
Christophe Rosset <tophe@topheman.com>
2021-05-13 17:36:34 +0200

    feat(apollo): add [owner]/[repository].tsx page

--- src/pages/[owner]/[repository].tsx
+++ src/pages/[owner]/[repository].tsx
@@ -1,18 +1,51 @@
 import { useRouter } from "next/router";
 import type { GetServerSideProps } from "next";
+import { gql, useQuery } from "@apollo/client";
 
+import type { PageBaseProps } from "../../types";
+import { initializeApollo, addApolloState } from "../../libs/apollo-client";
 import TheHeader from "../../components/TheHeader";
+
+const REPOSITORY_QUERY = gql`
+  query GetSpecificRepo($owner: String!, $repository: String!) {
+    repository(name: $repository, owner: $owner) {
+      primaryLanguage {
+        name
+      }
+      description
+      owner {
+        avatarUrl
+      }
+      forkCount
+      stargazerCount
+    }
+  }
+`;
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { owner, repository } = context.query;
-  console.log("owner", owner, "repository", repository);
-  console.log(context.resolvedUrl);
-  return {
-    props: {},
+  const baseProps = {
+    graphQLVariables: {
+      owner,
+      repository,
+    },
   };
+  const apolloClient = initializeApollo();
+  await apolloClient.query({
+    query: REPOSITORY_QUERY,
+    variables: baseProps.graphQLVariables,
+  });
+  return addApolloState(apolloClient, {
+    props: { ...baseProps },
+  });
 };
 
-export default function PageRepository() {
+export default function PageRepository({
+  graphQLVariables,
+}: PageBaseProps<{}>) {
+  const repositoryResult = useQuery(REPOSITORY_QUERY, {
+    variables: graphQLVariables,
+  });
   const router = useRouter();
   const { owner, repository } = router.query;
   return (
@@ -21,6 +54,10 @@ export default function PageRepository() {
         Owner: "{owner}" / Repository: "{repository}"
       </h1>
       <TheHeader />
+      <ul>
+        <li>{repositoryResult?.data?.repository.description}</li>
+        <li>{repositoryResult?.data?.repository.primaryLanguage.name}</li>
+      </ul>
     </>
   );
 }
