5c297a7cc3a7d5bfed9a7dc48b0b6337915cad7a
Christophe Rosset <tophe@topheman.com>
2021-05-24 19:18:52 +0200

    feat(tailwind): add brand colors to colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,11 @@
+const brandColors = {
+  "brand-primary": {
+    DEFAULT: "var(--color-brand-primary)",
+    light: "var(--color-brand-primary-light)",
+  },
+  "brand-secondary": "var(--color-brand-secondary)",
+};
+
 module.exports = {
   mode: "jit",
   purge: [
@@ -7,20 +15,19 @@ module.exports = {
   darkMode: false, // or 'media' or 'class'
   theme: {
     colors: {
-      "brand-primary": {
-        DEFAULT: "var(--color-brand-primary)",
-        light: "var(--color-brand-primary-light)",
-      },
-      "brand-secondary": "var(--color-brand-secondary)",
+      ...brandColors,
     },
     textColor: {
+      ...brandColors,
       primary: "var(--color-text-primary)",
       secondary: "var(--color-text-secondary)",
     },
     backgroundColor: {
+      ...brandColors,
       primary: "var(--color-bg-primary)",
     },
     borderColor: {
+      ...brandColors,
       primary: "var(--color-border-primary)",
       secondary: "var(--color-border-secondary)",
     },
