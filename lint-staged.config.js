31f1d72124df27b6b3ae8774fc4cfd750b14a898
Christophe Rosset <tophe@topheman.com>
2021-06-11 10:09:32 +0200

    chore(husky): add npm test on pre-commit

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  "*.{js,jsx,ts,tsx}": "eslint --cache --fix",
+  "*.{js,jsx,ts,tsx}": ["eslint --cache --fix", "npm run test:precommit"],
   "*{ts,tsx}": () => "tsc --project tsconfig.json --pretty --noEmit",
 };
