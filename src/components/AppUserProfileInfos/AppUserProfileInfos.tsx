48f08ba7f48a685401b2d3027a9331f1bd0fc8d1
Christophe Rosset <tophe@topheman.com>
2021-06-16 20:03:01 +0200

    feat(css): integration of AppUserProfileInfos

--- src/components/AppUserProfileInfos/AppUserProfileInfos.tsx
+++ src/components/AppUserProfileInfos/AppUserProfileInfos.tsx
@@ -25,66 +25,79 @@ export default function AppUserProfileInfos({
       itemType="http://schema.org/Person"
       className="text-primary bg-primary"
     >
-      <div>
-        <a itemProp="image" href={user.avatarUrl}>
-          <AppAvatarImage
-            avatarUrl={user.avatarUrl}
-            alt={user.name || "Avatar"}
-            rounded="full"
-          />
-        </a>
+      <div className="flex md:block items-center mb-4">
+        <div className="mr-2 md:-mr-0 w-20 md:w-auto">
+          <a itemProp="image" href={user.avatarUrl}>
+            <AppAvatarImage
+              avatarUrl={user.avatarUrl}
+              alt={user.name || "Avatar"}
+              rounded="full"
+            />
+          </a>
+        </div>
+        <div className="md:block w-full">
+          <h1>
+            <span itemProp="name" className="block text-2xl font-medium">
+              {user.name}
+            </span>
+            <span
+              itemProp="additionalName"
+              className="block font-light text-secondary"
+            >
+              {user.login}
+            </span>
+          </h1>
+        </div>
       </div>
-      <div>
-        <h1>
-          <span itemProp="name" className="text-2xl font-semibold">
-            {user.name}
-          </span>
-          <span itemProp="additionalName">{user.login}</span>
-        </h1>
-      </div>
-      <div>
-        <p>{user.bio}</p>
-        <ul>
+      <div className="flex flex-col">
+        <p className="mb-4">{user.bio}</p>
+        <ul className="order-1 md:order-none">
           {user.followers.totalCount ? (
-            <li>
-              <PeopleIcon /> {user.followers.totalCount} follower
-              {user.followers.totalCount > 1 ? "s" : ""}
+            <li className="inline mr-2">
+              <PeopleIcon /> {user.followers.totalCount}{" "}
+              <span className="text-secondary">
+                follower
+                {user.followers.totalCount > 1 ? "s" : ""}
+              </span>
             </li>
           ) : null}
           {user.following.totalCount ? (
-            <li>{user.following.totalCount} following</li>
+            <li className="inline mr-2">
+              {user.following.totalCount}{" "}
+              <span className="text-secondary">following</span>
+            </li>
           ) : null}
           {user.starredRepositories.totalCount > 0 ? (
-            <li>
+            <li className="inline mr-2">
               <StarIcon /> {user.starredRepositories.totalCount}
             </li>
           ) : null}
         </ul>
+        <ul>
+          {user.location ? (
+            <li className="hidden md:inline-block">
+              <LocationIcon /> {user.location}
+            </li>
+          ) : null}
+          {user.websiteUrl ? (
+            <li>
+              <a rel="nofollow me" href={user.websiteUrl}>
+                <LinkIcon /> {user.websiteUrl}
+              </a>
+            </li>
+          ) : null}
+          {user.twitterUsername ? (
+            <li className="hidden md:inline-block">
+              <a
+                rel="nofollow me"
+                href={`https://twitter.com/${user.twitterUsername}`}
+              >
+                <TwitterIcon /> @{user.twitterUsername}
+              </a>
+            </li>
+          ) : null}
+        </ul>
       </div>
-      <ul>
-        {user.location ? (
-          <li>
-            <LocationIcon /> {user.location}
-          </li>
-        ) : null}
-        {user.websiteUrl ? (
-          <li>
-            <a rel="nofollow me" href={user.websiteUrl}>
-              <LinkIcon /> {user.websiteUrl}
-            </a>
-          </li>
-        ) : null}
-        {user.twitterUsername ? (
-          <li>
-            <a
-              rel="nofollow me"
-              href={`https://twitter.com/${user.twitterUsername}`}
-            >
-              <TwitterIcon /> @{user.twitterUsername}
-            </a>
-          </li>
-        ) : null}
-      </ul>
     </div>
   );
 }
