651f9318db90ce280dfb995632da41b7a1599e28
Christophe Rosset <tophe@topheman.com>
2021-06-27 21:08:43 +0200

    feat(AppTag): add story

---
+++ src/components/AppTag/AppTag.stories.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Story, Meta } from "@storybook/react";
+
+import AppTag, { AppTagProps } from "./AppTag";
+
+export default {
+  title: "AppTag",
+  component: AppTag,
+} as Meta;
+
+const Template: Story<AppTagProps> = (args) => <AppTag {...args} />;
+
+export const Base = Template.bind({});
+Base.parameters = {};
+Base.args = {
+  ownerLogin: "topheman",
+  repositoryName: "nextjs-github-browser",
+  count: 12,
+  type: "stargazers",
+};
