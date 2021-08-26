231e6bee89c87c21885f83c0f8f891db69aacc7f
Christophe Rosset <tophe@topheman.com>
2021-08-26 18:29:30 +0200

    feat(Home): add wip message

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,7 +4,7 @@ import styles from "../styles/Home.module.css";
 
 export default function PageAbout(): JSX.Element {
   return (
-    <div className={styles.container}>
+    <div className={`${styles.container} text-primary`}>
       <Head>
         <title>nextjs-github-browser</title>
         <meta name="description" content="nextjs-github-browser" />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
 export default function PageIndex(): JSX.Element {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <Head>
+        <title>nextjs-github-browser</title>
+        <meta name="description" content="nextjs-github-browser" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={`${styles.main} text-primary`}>
+        <h1 className={styles.title}>nextjs-github-browser</h1>
+        <h2 className="mt-6 text-5xl">Work in progress</h2>
+        <p className="mt-6">
+          <span role="img" aria-label="Construction panel" className="text-9xl">
+            🚧
+          </span>
+        </p>
+        <p className="mt-6">
+          <a href="https://github.com/topheman" className="text-brand-primary">
+            @topheman
+          </a>
+        </p>
+      </main>
     </div>
   );
 }
