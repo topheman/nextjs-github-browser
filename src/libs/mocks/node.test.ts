91ffcfa560a50c97b271a2166f519847514a4ab0
Christophe Rosset <tophe@topheman.com>
2021-09-05 12:21:32 +0200

    refactor(mocks)

---
+++ src/libs/mocks/node.test.ts
@@ -0,0 +1,164 @@
+/* eslint-disable global-require,@typescript-eslint/no-var-requires */
+import { getMockFilePath, saveMock, loadMock } from "./node";
+
+function rootMockDirectory() {
+  return require("path").join(__dirname, "..", "..", "..", ".tmp/.mock-test");
+}
+
+const baseOptions = {
+  rootMockDirectory,
+  endpoint: "https://www.example.com/graphql",
+};
+
+describe("libs/mocks/node", () => {
+  describe("getMockFilePath", () => {
+    it("should correctly hash variables", () => {
+      const result = getMockFilePath(
+        "MyQueryName",
+        {
+          query: "some variable",
+          foo: "other variable",
+        },
+        baseOptions
+      );
+      expect(result).toContain(
+        ".tmp/.mock-test/www.example.com/graphql/MyQueryName_1246ca6d7128858e2b3f2d3ee06cde329cc20697e146355fd62af214af3b2484.json"
+      );
+    });
+    it("should give the same hash with variables passed in other order", () => {
+      const result = getMockFilePath(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+        },
+        baseOptions
+      );
+      expect(result).toContain(
+        ".tmp/.mock-test/www.example.com/graphql/MyQueryName_1246ca6d7128858e2b3f2d3ee06cde329cc20697e146355fd62af214af3b2484.json"
+      );
+    });
+    it("should give the same hash with undefined variables", () => {
+      const result = getMockFilePath(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        baseOptions
+      );
+      expect(result).toContain(
+        ".tmp/.mock-test/www.example.com/graphql/MyQueryName_1246ca6d7128858e2b3f2d3ee06cde329cc20697e146355fd62af214af3b2484.json"
+      );
+    });
+  });
+  describe("saveMock", () => {
+    beforeAll(async () => {
+      await require("fs/promises").rmdir(rootMockDirectory(), {
+        recursive: true,
+      });
+    });
+    it("should save mock in proper folder", async () => {
+      await saveMock(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        JSON.stringify({ someRequest: "foo" }),
+        JSON.stringify({ someResponse: "bar" }),
+        baseOptions
+      );
+    });
+  });
+  describe("loadMock", () => {
+    beforeAll(async () => {
+      await require("fs/promises").rmdir(rootMockDirectory(), {
+        recursive: true,
+      });
+      await saveMock(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        JSON.stringify({ someRequest: "foo" }),
+        JSON.stringify({ someResponse: "bar" }),
+        baseOptions
+      );
+    });
+    it("should load mock (response) in proper folder (with parsing)", async () => {
+      const result = await loadMock(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        baseOptions
+      );
+      expect(result).toStrictEqual({ someResponse: "bar" });
+    });
+    it("should load mock (response) in proper folder (without parsing) - return a buffer", async () => {
+      const result = await loadMock(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        {
+          ...baseOptions,
+          parse: false,
+        }
+      );
+      expect(result).toBe('{"someResponse":"bar"}');
+    });
+    it("should load mock (request) in proper folder (with parsing)", async () => {
+      const result = await loadMock(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        {
+          ...baseOptions,
+          isRequest: true,
+        }
+      );
+      expect(result).toStrictEqual({ someRequest: "foo" });
+    });
+    it("should load mock (request) in proper folder (without parsing) - return a buffer", async () => {
+      const result = await loadMock(
+        "MyQueryName",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        {
+          ...baseOptions,
+          parse: false,
+          isRequest: true,
+        }
+      );
+      expect(result).toBe('{"someRequest":"foo"}');
+    });
+    it("should return null if not found", async () => {
+      const result = await loadMock(
+        "MyQueryThatDoesntExist",
+        {
+          foo: "other variable",
+          query: "some variable",
+          someUndefinedVar: undefined,
+        },
+        baseOptions
+      );
+      expect(result).toBeNull();
+    });
+  });
+});
---
+++ src/libs/mocks/node.ts
@@ -0,0 +1,105 @@
+/* eslint-disable global-require,@typescript-eslint/no-var-requires,import/no-dynamic-require */
+/**
+ * Those are functions to be used server-side to:
+ * - record mocks (inside the /api/github/graphql handler) : `saveMock`
+ * - to setup mocks on a mock server / check api state : `loadMock`
+ */
+
+// using require so that it will work in a node process of cypress if needed
+const fsPromises = require("fs/promises");
+const path = require("path");
+
+function generateMockIdFromGraphqlVariables(
+  variables: Record<string, unknown>
+): string {
+  const serializedVariablesSortedByKeyWithoutUndefined = Object.entries(
+    variables
+  )
+    .filter(([, value]) => typeof value !== "undefined")
+    .sort(([a], [b]) => (a < b ? -1 : 1))
+    .map(([key, value]) => `${key}|${value}`)
+    .join("");
+  const hashed = require("hash.js")
+    .sha256()
+    .update(serializedVariablesSortedByKeyWithoutUndefined)
+    .digest("hex");
+  return hashed;
+}
+
+type ManageMockOptionsType = {
+  rootMockDirectory: () => string;
+  endpoint: string;
+};
+
+type GetMockFilePathOptionsType = ManageMockOptionsType & {
+  isRequest?: boolean;
+};
+
+export function getMockFilePath(
+  operationName: string,
+  variables: Record<string, unknown>,
+  { isRequest = false, rootMockDirectory, endpoint }: GetMockFilePathOptionsType
+): string {
+  const cleanEnpoint = endpoint.replace(/https?:\/\//, "");
+  return path.join(
+    rootMockDirectory(),
+    cleanEnpoint,
+    `${operationName}_${generateMockIdFromGraphqlVariables(variables)}${
+      isRequest ? "_request" : ""
+    }.json`
+  );
+}
+
+export async function saveMock(
+  operationName: string,
+  variables: Record<string, unknown>,
+  requestBody: string,
+  responseBody: string,
+  options: ManageMockOptionsType
+): Promise<string> {
+  const requestFilePath = getMockFilePath(operationName, variables, {
+    ...options,
+    isRequest: true,
+  });
+  const responseFilePath = getMockFilePath(operationName, variables, {
+    ...options,
+    isRequest: false,
+  });
+  const folderPath = path.dirname(requestFilePath);
+  await fsPromises.mkdir(folderPath, { recursive: true });
+  await Promise.all([
+    fsPromises.writeFile(requestFilePath, requestBody, "utf8"),
+    fsPromises.writeFile(responseFilePath, responseBody, "utf8"),
+  ]);
+  return responseFilePath;
+}
+
+export async function loadMock(
+  operationName: string,
+  variables: Record<string, unknown>,
+  {
+    parse = true,
+    ...getMockFilePathOptions
+  }: GetMockFilePathOptionsType & { parse?: boolean }
+): Promise<unknown | null> {
+  const filePath = getMockFilePath(
+    operationName,
+    variables,
+    getMockFilePathOptions
+  );
+  try {
+    // can't use `require` - it's not available via next
+    await fsPromises.stat(filePath); // check if file exists (readFile won't pass in catch)
+    const promise = fsPromises.readFile(filePath, "utf8");
+    if (!parse) {
+      return promise;
+    }
+    const result = await promise;
+    return JSON.parse(result.toString());
+  } catch (e) {
+    if (e.code === "ENOENT") {
+      return null;
+    }
+    throw e;
+  }
+}
