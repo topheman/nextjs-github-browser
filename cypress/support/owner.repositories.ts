9822230e4007b1f415955a64a9dd9bb8a9501cc6
Christophe Rosset <tophe@topheman.com>
2021-10-29 20:17:39 +0200

    test(cypress): skip check forward button on organization

--- cypress/support/owner.repositories.ts
+++ cypress/support/owner.repositories.ts
@@ -1,9 +1,12 @@
 export function runRepositoriesTests(
   url: string,
   login: string,
-  { searchQuery }: { searchQuery: string }
+  {
+    searchQuery,
+    skipCheckForwardButton,
+  }: { searchQuery: string; skipCheckForwardButton?: boolean }
 ): void {
-  describe("user/repositories", () => {
+  describe(url, () => {
     describe(`useSearchRepos - ${url}`, () => {
       it(`[SSR] should correctly render ${url}`, () => {
         cy.clientRepositoryAssertDefaultPage(url, login);
@@ -278,75 +281,78 @@ export function runRepositoriesTests(
         cy.url().should("contain", `${Cypress.config().baseUrl}${url}`);
         cy.location();
       });
-      it("[Client] should not break forward button", () => {
-        // fill the history
-        cy.clientRepositoryAssertDefaultPage(url, login);
-        cy.intercept("/api/github/graphql").as("graphql");
-        // without hitting cache
-        cy.clientRepositoryPaginationNavigate(
-          { direction: "next" },
-          false,
-          "all|last-updated|page2-with-after",
-          ({ query }) =>
-            expect(query).to.eq(`user:${login} sort:updated-desc fork:true`)
-        );
-        cy.clientRepositoryPaginationNavigate(
-          { direction: "next" },
-          false,
-          "all|last-updated|page3-with-after",
-          ({ query }) =>
-            expect(query).to.eq(`user:${login} sort:updated-desc fork:true`)
-        );
-        cy.clientRepositoryPaginationNavigate(
-          { type: "source" },
-          false,
-          "source|last-updated|page1-with-after",
-          ({ query }) =>
-            expect(query).to.eq(`user:${login} sort:updated-desc fork:false`)
-        );
-        cy.clientRepositoryPaginationNavigate(
-          {
-            custom: () => {
-              cy.go("back");
-              cy.waitBetweenNavigation();
+      (skipCheckForwardButton ? it.skip : it)(
+        "[Client] should not break forward button",
+        () => {
+          // fill the history
+          cy.clientRepositoryAssertDefaultPage(url, login);
+          cy.intercept("/api/github/graphql").as("graphql");
+          // without hitting cache
+          cy.clientRepositoryPaginationNavigate(
+            { direction: "next" },
+            false,
+            "all|last-updated|page2-with-after",
+            ({ query }) =>
+              expect(query).to.eq(`user:${login} sort:updated-desc fork:true`)
+          );
+          cy.clientRepositoryPaginationNavigate(
+            { direction: "next" },
+            false,
+            "all|last-updated|page3-with-after",
+            ({ query }) =>
+              expect(query).to.eq(`user:${login} sort:updated-desc fork:true`)
+          );
+          cy.clientRepositoryPaginationNavigate(
+            { type: "source" },
+            false,
+            "source|last-updated|page1-with-after",
+            ({ query }) =>
+              expect(query).to.eq(`user:${login} sort:updated-desc fork:false`)
+          );
+          cy.clientRepositoryPaginationNavigate(
+            {
+              custom: () => {
+                cy.go("back");
+                cy.waitBetweenNavigation();
+              },
             },
-          },
-          true,
-          "all|last-updated|page3-with-after"
-        );
-        cy.clientRepositoryPaginationNavigate(
-          {
-            custom: () => {
-              cy.go("back");
-              cy.waitBetweenNavigation();
+            true,
+            "all|last-updated|page3-with-after"
+          );
+          cy.clientRepositoryPaginationNavigate(
+            {
+              custom: () => {
+                cy.go("back");
+                cy.waitBetweenNavigation();
+              },
             },
-          },
-          true,
-          "all|last-updated|page2-with-after"
-        );
-        // unfortunately, cypress seems not to be able to do more than 2 cy.go('forward')
-        cy.clientRepositoryPaginationNavigate(
-          {
-            custom: () => {
-              cy.go("forward");
-              cy.waitBetweenNavigation();
+            true,
+            "all|last-updated|page2-with-after"
+          );
+          // unfortunately, cypress seems not to be able to do more than 2 cy.go('forward')
+          cy.clientRepositoryPaginationNavigate(
+            {
+              custom: () => {
+                cy.go("forward");
+                cy.waitBetweenNavigation();
+              },
             },
-          },
-          true,
-          "all|last-updated|page3-with-after"
-        );
-        cy.clientRepositoryPaginationNavigate(
-          {
-            custom: () => {
-              cy.go("forward");
-              cy.waitBetweenNavigation();
+            true,
+            "all|last-updated|page3-with-after"
+          );
+          cy.clientRepositoryPaginationNavigate(
+            {
+              custom: () => {
+                cy.go("forward");
+                cy.waitBetweenNavigation();
+              },
             },
-          },
-          true,
-          "source|last-updated|page1-with-after"
-        );
-        cy.location();
-      });
+            true,
+            "source|last-updated|page1-with-after"
+          );
+          cy.location();
+        }
+      );
     });
   });
 }
