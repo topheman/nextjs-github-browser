40671dafa915065a13fbb455807e9274b7a5aaf0
Christophe Rosset <tophe@topheman.com>
2021-11-01 10:03:48 +0100

    test(cypress): skipCheckForwardButton

--- cypress/integration/organization.repositories.spec.ts
+++ cypress/integration/organization.repositories.spec.ts
@@ -2,5 +2,5 @@ import { runRepositoriesTests } from "../support/owner.repositories";
 
 runRepositoriesTests("/orgs/twitter/repositories", "twitter", {
   searchQuery: "typeahead.js",
-  skipCheckForwardButton: true, // this case fails
+  skipCheckForwardButton: true, // this case fails on cypress (works manual testing)
 });
