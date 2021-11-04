a6b9ec454f16ea9a765094e35460004b30bc21d0
Christophe Rosset <tophe@topheman.com>
2021-11-04 18:55:03 +0100

    refactor([repositoryName].tsx)

--- src/pages/[owner]/[repositoryName]/tree/[...branchName].tsx
+++ src/pages/[owner]/[repositoryName]/tree/[...branchName].tsx
@@ -1,18 +1,8 @@
-import { useRouter } from "next/router";
+import {
+  makeGetServerSideProps,
+  makePage,
+} from "../../../../pages-shared/page-repository-tree";
 
-export default function PageRepositoryTree(): JSX.Element {
-  const router = useRouter();
-  const { owner, repositoryName, branchName } = router.query;
-  const resolvedBranchName = Array.isArray(branchName)
-    ? branchName.join("/")
-    : branchName;
-  return (
-    <>
-      <h1>Tree</h1>
-      <h2>
-        Owner: "{owner}" / Repository: "{repositoryName}"" / branchName: "
-        {resolvedBranchName}"
-      </h2>
-    </>
-  );
-}
+export const getServerSideProps = makeGetServerSideProps();
+
+export default makePage();
