d7d33f7ca66f723520000cb6d96224f55b74a35d
Christophe Rosset <tophe@topheman.com>
2021-12-02 19:07:19 +0100

    chore(*): remove remaining msw files (won't work due to HMR in NextJS)
    https://github.com/mswjs/msw/issues/642

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,21 +3,12 @@ import { AppProps } from "next/app";
 import { ApolloProvider } from "@apollo/client";
 import NextNprogress from "nextjs-progressbar";
 import { useApollo } from "../libs/apollo-client";
-import { parseBooleanEnvVar } from "../../utils";
 
 import "tailwindcss/tailwind.css";
 import "../styles/globals.css";
 import "github-markdown-css";
 import TheHeader from "../components/TheHeader/TheHeader";
 import TheFooter from "../components/TheFooter/TheFooter";
-
-if (parseBooleanEnvVar(process.env.NEXT_PUBLIC_API_MOCKS_ENABLED, false)) {
-  // eslint-disable-next-line no-console
-  console.warn(
-    "Mocks can't be enabled for the moment on NextJS due to : https://github.com/mswjs/msw/issues/642\nMSW mocks will only be used in unit tests"
-  );
-  // require("../mocks");
-}
 
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
   const apolloClient = useApollo(pageProps);
