b761c4ca5fd2fee3f61825c486ca6fea2e5f51c2
Christophe Rosset <tophe@topheman.com>
2021-12-02 20:19:51 +0100

    feat(Home): warn message about resetHelp only client-side

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,3 @@
-import { useEffect } from "react";
 import { useLocalStorage } from "../utils/hooks";
 
 import TheHome from "../components/TheHome/TheHome";
@@ -6,17 +5,15 @@ import TheHome from "../components/TheHome/TheHome";
 export default function PageIndex(): JSX.Element {
   const LOCALSTORAGE_KEY = "HELP_ENABLED";
   const [isHelpEnabled, enableHelp] = useLocalStorage(LOCALSTORAGE_KEY, true);
-  if (process.env.NODE_ENV === "development") {
+  if (process.env.NODE_ENV === "development" && typeof window !== "undefined") {
     // eslint-disable-next-line no-console
     console.log(
       `[DEV] Help is controled by the localStorage key "${LOCALSTORAGE_KEY}", to reset it you can call resetHelp() in the console (only available in dev mode)`
     );
-  }
-  useEffect(() => {
     (window as Window &
       typeof globalThis & { resetHelp: () => void }).resetHelp = () =>
       enableHelp(true);
-  }, [enableHelp]);
+  }
   return (
     <TheHome helpActive={isHelpEnabled} onExit={() => enableHelp(false)} />
   );
